0026384e61429e2f833777fdb665e4c2af5c87df
jpretel <jpretel@jpretel-pc.NISIRA.COM.PE>
2014-02-19 18:45:38 -0500

    String Utils

---
+++ sys/src/vista/utils/StringUtils.java
@@ -0,0 +1,24 @@
+package vista.utils;
+
+public class StringUtils {
+	public static String _left(String cadena, int longitud){
+		cadena = cadena.trim();
+		if (cadena.length() <= longitud)
+			return cadena;
+		return cadena.substring(0, longitud);
+	}
+	
+	public static String _right(String cadena, int longitud) {
+		cadena = cadena.trim();
+		
+		cadena = cadena.trim();
+		if (cadena.length() <= longitud)
+			return cadena;
+		return cadena.substring(cadena.length()-longitud);
+	}
+	
+	public static void main(String[] args) {
+		String cadena = "Holaaabbb";
+		System.out.println(_left(cadena, 3));
+	}
+}
