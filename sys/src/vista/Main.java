54cbc0e1bb22533653b8521680ced73868931461
jpretel <jpretel@jpretel-pc.NISIRA.COM.PE>
2014-02-21 21:21:44 -0500

    Prueba :)

---
+++ sys/src/vista/Main.java
@@ -0,0 +1,63 @@
+package vista;
+
+import java.awt.EventQueue;
+
+
+
+
+import org.jvnet.substance.SubstanceLookAndFeel;
+import org.jvnet.substance.skin.OfficeBlue2007Skin;
+
+import controlador.VariablesGlobales;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		try {	
+			salir:
+			for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager
+					.getInstalledLookAndFeels()) {
+				System.out.println(info.getName());
+				switch (info.getName()) {
+				case "GTK+":
+					javax.swing.UIManager.setLookAndFeel(info.getClassName());
+					break salir;
+				case "Windows":
+					javax.swing.UIManager.setLookAndFeel(info.getClassName());
+					break salir;
+				default:
+					javax.swing.UIManager
+							.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+					break;
+				}
+			}
+		} catch (ClassNotFoundException | InstantiationException
+				| IllegalAccessException
+				| javax.swing.UnsupportedLookAndFeelException ex) {
+		}
+<<<<<<< HEAD
+		aa
+		// Comentars
+=======
+		
+		// Comentarsss
+>>>>>>> branch 'master' of ssh://git@github.com/jpretel/sys.git
+		*/
+		
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SubstanceLookAndFeel.setSkin(new OfficeBlue2007Skin());
+					Home app = new Home("Sistema ERP");
+					VariablesGlobales.home = app;
+					app.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
