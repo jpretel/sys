13a9de2e0541de322d90a513ed1ce8d8c3979b1d
John Joseph Pretel Ballena <johnjoseph30.91@gmail.com>
2014-07-06 02:03:30 +0200

    Update AbstractDAO.java
    Cambio en clase abstracta

--- sys/src/dao/AbstractDAO.java
+++ sys/src/dao/AbstractDAO.java
@@ -12,6 +12,9 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
+/*
+Clase abstract
+*/
 public abstract class AbstractDAO<T> {
 	private Class<T> entityClass;
 
