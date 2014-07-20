af359404ad10060be35fe932c8bbf843a2be2c28
jpretel <jpretel@John-PC.NISIRA.COM.PE>
2014-07-20 14:01:19 -0500

    Mandenedor de Flujo

---
+++ sys/src/dao/FlujoDAO.java
@@ -0,0 +1,11 @@
+package dao;
+
+import entity.Flujo;
+
+public class FlujoDAO extends AbstractDAO<Flujo> {
+
+	public FlujoDAO() {
+		super(Flujo.class);
+	}
+
+}
---
+++ sys/src/entity/Flujo.java
@@ -0,0 +1,67 @@
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.List;
+
+
+/**
+ * The persistent class for the flujo database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Flujo.findAll", query="SELECT f FROM Flujo f")
+public class Flujo implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private String idflujo;
+
+	private String descripcion;
+
+	//bi-directional many-to-one association to SysDocFlujo
+	@OneToMany(mappedBy="flujo")
+	private List<SysDocFlujo> sysDocFlujos;
+
+	public Flujo() {
+	}
+
+	public String getIdflujo() {
+		return this.idflujo;
+	}
+
+	public void setIdflujo(String idflujo) {
+		this.idflujo = idflujo;
+	}
+
+	public String getDescripcion() {
+		return this.descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public List<SysDocFlujo> getSysDocFlujos() {
+		return this.sysDocFlujos;
+	}
+
+	public void setSysDocFlujos(List<SysDocFlujo> sysDocFlujos) {
+		this.sysDocFlujos = sysDocFlujos;
+	}
+
+	public SysDocFlujo addSysDocFlujo(SysDocFlujo sysDocFlujo) {
+		getSysDocFlujos().add(sysDocFlujo);
+		sysDocFlujo.setFlujo(this);
+
+		return sysDocFlujo;
+	}
+
+	public SysDocFlujo removeSysDocFlujo(SysDocFlujo sysDocFlujo) {
+		getSysDocFlujos().remove(sysDocFlujo);
+		sysDocFlujo.setFlujo(null);
+
+		return sysDocFlujo;
+	}
+
+}
