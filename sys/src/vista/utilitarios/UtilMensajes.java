bcef6980478f58d32b4c5884f5b293b629497bf5
jpretel <jpretel@John-PC.NISIRA.COM.PE>
2014-08-16 14:56:35 -0500

    Mensajes

--- sys/src/vista/utilitarios/UtilMensajes.java
+++ sys/src/vista/utilitarios/UtilMensajes.java
@@ -12,8 +12,20 @@ public class UtilMensajes {
 				titulo, JOptionPane.ERROR_MESSAGE);
 	}
 	
+	public static void mensaje_error(String mensaje, String... params){
+		String grandTotal = String.format(Sys.mensajes.getProperty(mensaje), params);
+		JOptionPane.showMessageDialog(null, grandTotal,
+				titulo, JOptionPane.ERROR_MESSAGE);
+	}
+	
 	public static void mensaje_alterta(String mensaje) {
 		JOptionPane.showMessageDialog(null, Sys.mensajes.getProperty(mensaje),
+				titulo, JOptionPane.INFORMATION_MESSAGE);
+	}
+	
+	public static void mensaje_alterta(String mensaje, String... params){
+		String grandTotal = String.format(Sys.mensajes.getProperty(mensaje), params);
+		JOptionPane.showMessageDialog(null, grandTotal,
 				titulo, JOptionPane.INFORMATION_MESSAGE);
 	}
 }
