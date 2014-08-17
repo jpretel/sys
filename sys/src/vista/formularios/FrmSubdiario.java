dfe33ca72a5fba8f4778b89fa1d06f26a0b2cd46
SERVIDOR PRINCIPAL <SERVIDOR PRINCIPAL@192.168.1.33>
2014-08-17 18:51:14 -0500

    Subdiaro

--- sys/src/vista/formularios/FrmSubdiario.java
+++ sys/src/vista/formularios/FrmSubdiario.java
@@ -8,6 +8,7 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 
+import vista.controles.JTextFieldLimit;
 import vista.utilitarios.MaestroTableModel;
 import vista.utilitarios.UtilMensajes;
 
@@ -47,7 +48,8 @@ public class FrmSubdiario extends AbstractMaestro {
 		txtCodigo = new JTextField();
 		txtCodigo.setBounds(276, 8, 122, 20);
 		txtCodigo.setColumns(10);
-
+		txtCodigo.setDocument(new JTextFieldLimit(3, true));
+		
 		JLabel lblDescripcin = new JLabel("Descripci\u00F3n");
 		lblDescripcin.setBounds(227, 36, 75, 14);
 
@@ -61,6 +63,7 @@ public class FrmSubdiario extends AbstractMaestro {
 		txtDescripcion = new JTextField();
 		txtDescripcion.setColumns(10);
 		txtDescripcion.setBounds(286, 33, 122, 20);
+		txtDescripcion.setDocument(new JTextFieldLimit(75, true));
 		
 		chkEsDeclarable = new JCheckBox("Es declarable");
 
