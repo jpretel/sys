b406e91044c873049f0d4998fd66a8ab758ac19e
jpretel <jpretel@John-PC.NISIRA.COM.PE>
2014-08-20 19:55:54 -0500

    table

--- sys/src/vista/utilitarios/JTableUtils.java
+++ sys/src/vista/utilitarios/JTableUtils.java
@@ -4,12 +4,14 @@ import java.awt.Component;
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Point;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.Vector;
 
 import javax.swing.AbstractListModel;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.ListCellRenderer;
 import javax.swing.ListModel;
@@ -17,15 +19,20 @@ import javax.swing.UIManager;
 import javax.swing.event.MouseInputAdapter;
 import javax.swing.table.JTableHeader;
 
+import vista.controles.DSGTable;
+import vista.controles.DSGTableModel;
+
 public class JTableUtils {
 	private static final int MIN_ROW_HEIGHT = 12;
 
-	public static JList buildRowHeader(final JTable table) {
+	public static JList buildRowHeader(final JTable table, DSGTableModel model) {
 		final Vector<String> headers = new Vector<String>();
 		for (int i = 0; i < table.getRowCount(); i++) {
-			//headers.add(String.valueOf((char) (i + 65)).toUpperCase());
-			headers.add(String.valueOf(i+1).toUpperCase());
+			// headers.add(String.valueOf((char) (i + 65)).toUpperCase());
+			headers.add(String.valueOf(i + 1).toUpperCase());
 		}
+
+		headers.add("+");
 		ListModel lm = new AbstractListModel() {
 
 			public int getSize() {
@@ -36,38 +43,17 @@ public class JTableUtils {
 				return headers.get(index);
 			}
 		};
-		
+
 		final JList rowHeader = new JList(lm);
 		rowHeader.setOpaque(false);
 		rowHeader.setFixedCellWidth(30);
-		
+
 		MouseInputAdapter mouseAdapter = new MouseInputAdapter() {
 			Cursor oldCursor;
 			Cursor RESIZE_CURSOR = Cursor
 					.getPredefinedCursor(Cursor.S_RESIZE_CURSOR);
 			int index = -1;
 			int oldY = -1;
-
-			@Override
-			public void mousePressed(MouseEvent e) {
-				super.mousePressed(e);
-			}
-
-			@Override
-			public void mouseMoved(MouseEvent e) {
-				super.mouseMoved(e);
-				int previ = getLocationToIndex(new Point(e.getX(), e.getY() - 3));
-				int nexti = getLocationToIndex(new Point(e.getX(), e.getY() + 3));
-				if (previ != -1 && previ != nexti) {
-					if (!isResizeCursor()) {
-						oldCursor = rowHeader.getCursor();
-						rowHeader.setCursor(RESIZE_CURSOR);
-						index = previ;
-					}
-				} else if (isResizeCursor()) {
-					rowHeader.setCursor(oldCursor);
-				}
-			}
 
 			private int getLocationToIndex(Point point) {
 				int i = rowHeader.locationToIndex(point);
@@ -78,52 +64,77 @@ public class JTableUtils {
 			}
 
 			@Override
-			public void mouseReleased(MouseEvent e) {
-				super.mouseReleased(e);
-				if (isResizeCursor()) {
-					rowHeader.setCursor(oldCursor);
-					index = -1;
-					oldY = -1;
-				}
-			}
+			public void mousePressed(MouseEvent e) {
+				super.mousePressed(e);
+				int previ = getLocationToIndex(new Point(e.getX(), e.getY() - 3));
+				if (model.getEditar()) {
+					if (previ == table.getRowCount()) {
+						model.addRow();
+					} else if (previ > -1 && previ < table.getRowCount()
+							&& e.getClickCount() == 2) {
 
-			@Override
-			public void mouseDragged(MouseEvent e) {
-				super.mouseDragged(e);
-				if (isResizeCursor() && index != -1) {
-					int y = e.getY();
-					if (oldY != -1) {
-						int inc = y - oldY;
-						int oldRowHeight = table.getRowHeight(index);
-						if (oldRowHeight > 12 || inc > 0) {
-							int rowHeight = Math.max(MIN_ROW_HEIGHT,
-									oldRowHeight + inc);
-							table.setRowHeight(index, rowHeight);
-							if (rowHeader.getModel().getSize() > index + 1) {
-								int rowHeight1 = table.getRowHeight(index + 1)
-										- inc;
-								rowHeight1 = Math.max(12, rowHeight1);
-								table.setRowHeight(index + 1, rowHeight1);
-							}
+						int seleccion = JOptionPane.showOptionDialog(null,
+								"Desea Eliminar el Registro Seleccionado",
+								"Informacion del Sistema",
+								JOptionPane.YES_NO_OPTION,
+								JOptionPane.QUESTION_MESSAGE, null,
+								new Object[] { "Si", "No" }, "Si");
+						if (seleccion == 0) {
+							model.removeRow(previ);
 						}
 					}
-					oldY = y;
 				}
 			}
 
 			private boolean isResizeCursor() {
 				return rowHeader.getCursor() == RESIZE_CURSOR;
 			}
+
+			/*
+			 * (non-Javadoc)
+			 * 
+			 * @see
+			 * java.awt.event.MouseAdapter#mouseMoved(java.awt.event.MouseEvent)
+			 */
+			/*
+			 * @Override public void mouseMoved(MouseEvent e) {
+			 * super.mouseMoved(e); int previ = getLocationToIndex(new
+			 * Point(e.getX(), e.getY() - 3)); int nexti =
+			 * getLocationToIndex(new Point(e.getX(), e.getY() + 3)); if (previ
+			 * != -1 && previ != nexti) { if (!isResizeCursor()) { oldCursor =
+			 * rowHeader.getCursor(); rowHeader.setCursor(RESIZE_CURSOR); index
+			 * = previ; } } else if (isResizeCursor()) {
+			 * rowHeader.setCursor(oldCursor); } }
+			 * 
+			 * @Override public void mouseReleased(MouseEvent e) {
+			 * super.mouseReleased(e); if (isResizeCursor()) {
+			 * rowHeader.setCursor(oldCursor); index = -1; oldY = -1; } }
+			 * 
+			 * @Override public void mouseDragged(MouseEvent e) {
+			 * super.mouseDragged(e); if (isResizeCursor() && index != -1) { int
+			 * y = e.getY(); if (oldY != -1) { int inc = y - oldY; int
+			 * oldRowHeight = table.getRowHeight(index);
+			 * 
+			 * if (oldRowHeight > 12 || inc > 0) { int rowHeight =
+			 * Math.max(MIN_ROW_HEIGHT, oldRowHeight + inc);
+			 * 
+			 * table.setRowHeight(index, rowHeight);
+			 * 
+			 * if (rowHeader.getModel().getSize() > index + 1) { int rowHeight1
+			 * = table.getRowHeight(index + 1) - inc; rowHeight1 = Math.max(12,
+			 * rowHeight1); table.setRowHeight(index + 1, rowHeight1); } }
+			 * 
+			 * } oldY = y; } }
+			 */
+
 		};
-		
 		rowHeader.addMouseListener(mouseAdapter);
 		rowHeader.addMouseMotionListener(mouseAdapter);
 		rowHeader.addMouseWheelListener(mouseAdapter);
-		
-		rowHeader.setCellRenderer(new RowHeaderRenderer(table));
+		rowHeader.setCellRenderer(new RowHeaderRenderer(table, model));
 		rowHeader.setBackground(table.getBackground());
 		rowHeader.setForeground(table.getForeground());
-		
+
 		return rowHeader;
 	}
 
@@ -131,7 +142,7 @@ public class JTableUtils {
 
 		private JTable table;
 
-		RowHeaderRenderer(JTable table) {
+		public RowHeaderRenderer(JTable table, DSGTableModel model) {
 			this.table = table;
 			JTableHeader header = this.table.getTableHeader();
 			setOpaque(true);
