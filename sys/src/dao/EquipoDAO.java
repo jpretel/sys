2ece4e4dab7fe6247f191c0c81fe14a220ff8c37
Leo <Leo@DELL>
2014-09-28 22:00:56 -0500

    Mantenedor Equipos

---
+++ sys/src/dao/EquipoDAO.java
@@ -0,0 +1,12 @@
+package dao;
+
+import entity.Equipo;
+
+public class EquipoDAO extends AbstractDAO<Equipo>{
+
+	public EquipoDAO() {
+		super(Equipo.class);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ sys/src/dao/SubEquipoDAO.java
@@ -0,0 +1,27 @@
+package dao;
+
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaDelete;
+import javax.persistence.criteria.Root;
+
+import entity.Equipo;
+import entity.Subequipo;
+
+public class SubEquipoDAO extends AbstractDAO<Subequipo>{
+
+	public SubEquipoDAO() {
+		super(Subequipo.class);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void borrarPorEquipo (Equipo equipo) {
+		getEntityManager().getTransaction().begin();
+		CriteriaDelete<Subequipo> delete = cb.createCriteriaDelete(Subequipo.class);
+		Root<Subequipo> c = delete.from(Subequipo.class);
+		delete.where(cb.equal(c.get("equipo"), equipo));
+		Query query = getEntityManager().createQuery(delete);
+		query.executeUpdate();
+		getEntityManager().getTransaction().commit();
+	}
+
+}
