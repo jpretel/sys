6faf28df0931143d5d8eb36d4cd4436c657297b4
jpretel <jpretel@JPRETELB.NISIRA.COM.PE>
2014-10-02 07:32:02 -0500

    Borrar Tree Table

--- sys/src/treetabletest/p3/ChildNode.java
+++
@@ -1,7 +0,0 @@
-package treetabletest.p3;
-
-public class ChildNode extends Node {
-	public ChildNode(Object[] data) {
-		super(data);
-	}
-}
--- sys/src/treetabletest/p3/RootNode.java
+++
@@ -1,8 +0,0 @@
-package treetabletest.p3;
-
-public class RootNode extends Node {
-	public RootNode(String key) {
-		super(new Object[] { key });
-	}
-
-}
